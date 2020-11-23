4662e80c47c9ee6c61f57739b4870234374b8894
alan358 <74546182+alan358@users.noreply.github.com>
2020-11-22 20:23:02 -0500

    Update client.py

--- client.py
+++ client.py
@@ -4,17 +4,16 @@ import socket
 import sys
 
 def nickname():
-	true = "true"
-	while true == "true":
-		for msg in Lib.RecvMessage(s):
-			print "Please input a nickname"
-			Message = raw_input()
-			Lib.SendMessage(s, "NICK %s" %(Message))
-	for msg in Lib.RecvMessage(connection):
-		if msg != "READY":
-			true = "true"
-		else:
-			true = "False"
+	correctnickname = "false"
+	while correctnickname == "false":
+		print "Please input a nickname"
+		Message = raw_input()
+		Lib.SendMessage(s, "NICK %s" %(Message))
+		for msg in Lib.RecvMessage(connection):
+			if msg != "READY":
+				correctnickname = "false"
+			else:
+				correctnickname = "true"
 	
 	
 	
@@ -22,5 +21,5 @@ def nickname():
 s = Lib.CreateConnect(sys.argv[1],sys.argv[2])
 Lib.SendMessage(s, "Hello")
 for msg in Lib.RecvMessage(s):
-    if msg == "NICK":
+	if msg == "HELLO":
 		nickname()
