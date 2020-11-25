0b136156758dc5417121a21f936151266a3b92e3
alan358 <74546182+alan358@users.noreply.github.com>
2020-11-24 21:42:39 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,3 +1,15 @@
+#Author:          Alan Michener
+#Major:           Computer Science
+#Creation Date:   november 17, 2020
+#Due Date:        november 24, 2020
+#Course:          Csc328
+#Professors name: Dr. Frye
+#Assignment:      chatserver
+#Filename:        client.py
+#Purpose:         This connects a client code with server code and begins communication
+#Language and version: python version 3.7.7 the one on acad
+#Compile and execute: python qotd_AlanMichener.py udp djxmmx.net
+#with changing udp to tcp and changing djxmmx.net to another server
 import Lib, socket, sys
 
 def nickname(con):
@@ -18,8 +30,12 @@ print("connection started successfully. ")
 
 # set nickname 
 nickname(s)
-
+print("You are not allowed to send messages to the server if at anytime you want to end please just type BYE in the chat.")
+try:
 #message prompt
-while True:
-    user_message = input("--> ")
-    Lib.SendMessage(s, user_message)
+    while True:
+        user_message = input("--> ")
+        Lib.SendMessage(s, user_message)
+       
+except user_message == "BYE":
+    Lib.SendMessage(s, user_message, msgtype="BYE")
